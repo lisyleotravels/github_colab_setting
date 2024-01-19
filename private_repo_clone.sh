9baa304117a60d0c43acd0b7735811e1dcb6430e
lehidalgobbva <152307866+lehidalgobbva@users.noreply.github.com>
2024-01-19 13:03:36 +0100

    Update private_repo_clone.sh

--- private_repo_clone.sh
+++ private_repo_clone.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+echo "Generating RSA Key..."
 ssh-keygen -t rsa -b 4096
+
+echo "Adding GitHub to known hosts..."
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
-git config --global user.email $1
-git config --global user.name $2
+
+echo "Configuring Git with provided email and username..."
+git config --global user.email "$1"
+git config --global user.name "$2"
+
+echo "Your SSH public key (id_rsa.pub) is:"
+echo "================================================================"
 cat /root/.ssh/id_rsa.pub
+echo "================================================================"
+echo "Please copy the above SSH public key and add it to your GitHub account."
